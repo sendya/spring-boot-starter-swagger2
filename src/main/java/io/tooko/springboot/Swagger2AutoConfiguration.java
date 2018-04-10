6626428efe2d8332620614e6fdd9a162ea7037d4
Tooko <18x@loacg.com>
2018-04-10 09:37:24 +0800

    change swagger-ui version

--- src/main/java/io/tooko/springboot/Swagger2AutoConfiguration.java
+++ src/main/java/io/tooko/springboot/Swagger2AutoConfiguration.java
@@ -3,6 +3,7 @@ package io.tooko.springboot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,6 +14,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @ComponentScan
+@Configuration
 @EnableSwagger2
 public class Swagger2AutoConfiguration {
 
